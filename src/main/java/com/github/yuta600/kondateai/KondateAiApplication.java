6724f374d1ace67e7a76b25a989b884717e6cd80
Yuta.K <you.1014ta@gmail.com>
2025-11-15 14:37:50 +0900

    chore: SpringBootプロジェクトのセットアップ

---
+++ src/main/java/com/github/yuta600/kondateai/KondateAiApplication.java
@@ -0,0 +1,13 @@
+package com.github.yuta600.kondateai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KondateAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KondateAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yuta600/kondateai/KondateAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yuta600.kondateai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KondateAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
